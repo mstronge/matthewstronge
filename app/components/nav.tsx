a61d3654e593d63e857251e296a18b4262599de5
Matt Stronge <me@matthewstronge.com>
2024-10-21 22:07:39 +0100

    Minor nav changes

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import { IoCalendarSharp, IoHomeSharp, IoPencilSharp } from "react-icons/io5";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const navItems = {
   '/': {
@@ -9,6 +11,26 @@ const navItems = {
   '/blog': {
     name: 'blog',
     icon: <IoPencilSharp />,
+  },
+  'https://www.github.com/mstronge': {
+    name: 'github',
+    target: '_blank',
+    icon: <FaGithub />,
+  },
+  'https://www.linkedin.com/in/matt-stronge': {
+    name: 'linkedin',
+    target: '_blank',
+    icon: <FaLinkedin />,
+  },
+  'https://www.instagram.com/matthewstronge': {
+    name: 'instagram',
+    target: '_blank',
+    icon: <FaInstagram />,
+  },
+  'https://x.com/strongematthew': {
+    name: 'x',
+    target: '_blank',
+    icon: <FaXTwitter />,
   },
 }
 
@@ -20,19 +42,20 @@ export function Navbar() {
           className="flex w-full relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10 w-full">
-            {Object.entries(navItems).map(([path, { icon }]) => {
+          <div className="flex flex-row space-x-0 w-full">
+            {Object.entries(navItems).map(([path, { icon, target }]) => {
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 hover:scale-150 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  target={target}
+                  className="transition-all items-center hover:text-neutral-800 hover:scale-150 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
                 >
                   {icon}
                 </Link>
               )
             })}
-            <a target='_blank' className="transition-all self-end hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle justify-end items-center flex-auto py-1 px-2 m-1 space-x-4" href="https://www.cal.com/matthewstronge"><IoCalendarSharp /> <span className='font-bold'>Schedule a call</span></a>
+            <a target='_blank' className="transition-all self-end hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle justify-end items-center flex-auto py-1 px-2 m-1 space-x-4" href="https://cal.com/matthewstronge?redirect=false"><IoCalendarSharp /> <span className='font-bold hidden sm:block'>Schedule a call</span></a>
           </div>
         </nav>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,40 @@
-import { BlogPosts } from 'app/components/posts'
-import Image from 'next/image';
-import me from '../public/images/matthew-stronge-small.png'
+  import { BlogPosts } from 'app/components/posts'
+  import Image from 'next/image';
+  import me from '../public/images/matthew-stronge-small.png'
 
-export default function Page() {
-  return (
-    <section>
-      <div className="flex flex-col justify-center align-middle mb-12 md:flex-row">
-        <Image alt="Matthew Stronge" src={me} className='w-36 h-36 rounded-full' />
-        <div className='pl-0 pt-8 sm:pl-12 sm:pt-0 space-y-1'>
-          <h1 className="mb-4 text-4xl m-0 p-0 font-semibold">
-          Matthew Stronge
-          </h1>
-          <h2 className="mb-4 text-xl font-semibold">
-            Father, software engineer and founder/CEO.</h2>
-          <p className="mb-0 pb-0">  I build things with NodeJS/React/Javascript. <em>Sometimes</em> I write about <strong>Javascript</strong>, <strong>AI</strong> and its utilisation within <strong>financial technology.</strong>.
-          </p>
+  export default function Page() {
+    return (
+      <section>
+        <div className="flex flex-col justify-center align-middle mb-12 md:flex-row">
+          <Image alt="Matthew Stronge" src={me} className='w-36 h-36 rounded-full' />
+          <div className='pl-0 pt-8 sm:pl-12 sm:pt-0 space-y-1'>
+            <h1 className="mb-4 text-4xl m-0 p-0 font-semibold">
+            Matthew Stronge
+            </h1>
+            <h2 className="mb-4 text-xl font-semibold">
+              Father, software engineer and founder/CEO.</h2>
+            <p className="mb-0 pb-0">  I build things with NodeJS/React/Javascript. <em>Sometimes</em> I write about <strong>Javascript</strong>, <strong>AI</strong> and its utilisation within <strong>financial technology.</strong>.
+            </p>
+          </div>
         </div>
+
+      <div className="my-8">
+        <h2 className="mb-4 text-xl font-semibold">Blog</h2>
+        <BlogPosts />
       </div>
 
       <div className="my-8">
-        <BlogPosts />
+      <h2 className="mb-4 text-xl font-semibold">Recent work</h2>
       </div>
-    </section>
-  )
-}
+
+      <div className="my-8">
+        <h2 className="mb-4 text-xl font-semibold">Companies</h2>
+      </div>
+
+      <div className="my-8">
+        <h2 className="mb-4 text-xl font-semibold">Open source</h2>
+      </div>
+
+      </section>
+    )
+  }
